ae22a2dbf90550121608c59e3ea8a7f88370000c
Christopher <cgfandrich@outlook.com>
2023-09-28 20:16:13 -0700

    Add some more memory functions + added the size_t and ssize_t types

--- src/utils/memory.h
+++ src/utils/memory.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <types.h>
+
 /// @brief Copies data from one memory location to another
 /// @param src Source memory location (data to be copied)
 /// @param dst Destination memory location (place for data to be copied to)
 void strcpy(char* src, char* dst);
+
+/// @brief Copies memory from one location to another, this does not handle overlapping memory regions
+/// @param src Source memory location (data to be copied)
+/// @param dst Destination memory location (place for data to be copied to)
+/// @param count The amount of bytes to be copied
+void memcpy(const void* restrict src, void* restrict dst, size_t count);
+
+/// @brief Copies memory from one location to another, this handles overlapping memory regions
+/// @param src Source memory location (data to be copied)
+/// @param dst Destination memory location (place for data to be copied to)
+/// @param count The amount of bytes to be copied
+void memmove(const void* src, void* dst, size_t count);
